8f39346c78b902aadb37cedf89ddfa593590c218
MANN GWAL <manngwal2580@gmail.com>
2024-10-11 17:36:45 +0530

    init commit

--- Sorting/src/Inbuilt_Sort.java
+++ Sorting/src/Inbuilt_Sort.java
@@ -1,14 +1,24 @@
 import java.util.Arrays;
+import java.util.Collections;
 public class Inbuilt_Sort {
     public static void print(int[] sample){
         for (int i = 0; i < sample.length; i++){
             System.out.print(sample[i] + " ");
         }
     }
+    public static void printColl(Integer[] sample){
+        for (int i = 0; i < sample.length; i++){
+            System.out.print(sample[i] + " ");
+        }
+    }
     public static void main(String[] args){
         int[] sample = {5, 4, 1, 3, 2};
+        Integer[] Samples = {4, 3, 1, 2, 5};
         Arrays.sort(sample, 0, 3);
         print(sample);
+        System.out.println();
+        Arrays.sort(Samples, Collections.reverseOrder());
+        printColl(Samples);
         System.out.println();
         Arrays.sort(sample);
         print(sample);
